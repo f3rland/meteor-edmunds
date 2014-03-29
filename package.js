0c11d065f680ebdb2ab8015f6823d3040e79df96
f3rland <f3rland@gmail.com>
2014-03-29 11:36:43 -0400

    Fix in package

--- package.js
+++ package.js
@@ -6,6 +6,10 @@ Package.on_use(function (api) {
 	var path = Npm.require('path');
 	var asset_path = path.join('lib');
 	
-	api.add_files(path.join(asset_path, 'edmunds.api.sdk.js'), 'client');	
+	api.add_files(path.join(asset_path, 'edmunds.api.sdk.js'), 'client');
+
+	if (api.export) {
+		api.export("EDMUNDSAPI", "client");
+	}
 });
 
