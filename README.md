6a12c7f48184d73c9dd30811b3a682aee375bdde
f3rland <f3rland@gmail.com>
2014-03-29 11:07:14 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Meteor Edmund's API
+==========
+
+Meteor packaging for [Edmund's API](https://github.com/EdmundsAPI/sdk-javascript).
+
+``` sh
+$ meteor add edmunds
+```
---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  summary: "Meteor Edmund's API."
+});
+
+Package.on_use(function (api) {
+	var path = Npm.require('path');
+	var asset_path = path.join('lib');
+	
+	api.add_files(path.join(asset_path, 'edmunds.api.sdk.js'), 'client');	
+});
+
